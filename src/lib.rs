9694b5e008d02bfd3bb94bbd3c07f16251176bf0
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2018-06-29 18:57:28 +0200

    Add `IterStatusExt::with_status` and related types

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+use std::{
+    iter::{FusedIterator, Peekable},
+};
+
 /// Allows you to always do something, except the first time.
 ///
 /// Internally, this is simply a `bool`. It stores whether
@@ -71,5 +75,138 @@ impl SkipFirst {
         } else {
             f();
         }
+    }
+}
+
+/// Iterator wrapper which keeps track of the status. See
+/// [`IterStatusExt::with_status`] for more information.
+pub struct WithStatus<I: Iterator> {
+    iter: Peekable<I>,
+    first: bool,
+}
+
+impl<I: Iterator> WithStatus<I> {
+    fn new(iter: I) -> Self {
+        Self {
+            iter: iter.peekable(),
+            first: true,
+        }
+    }
+}
+
+impl<I: Iterator> Iterator for WithStatus<I> {
+    type Item = (I::Item, Status);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        // Get the next item from the iterator.
+        let item = self.iter.next();
+
+        let status = Status {
+            first: self.first,
+            // Since we already got the real item above, we can now peek if
+            // there is still another item.
+            last: self.iter.peek().is_none(),
+        };
+
+        if self.first {
+            self.first = false;
+        }
+
+        item.map(|elem| (elem, status))
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        // We pass through the `size_hint` method, as the underlying iterator
+        // might have size information.
+        self.iter.size_hint()
+    }
+}
+
+// Implement traits when the underlying iterator implements them.
+impl<I: FusedIterator> FusedIterator for WithStatus<I> {}
+impl<I: ExactSizeIterator> ExactSizeIterator for WithStatus<I> {
+    fn len(&self) -> usize {
+        self.iter.len()
+    }
+}
+
+/// Adds the `with_status` method to all iterators.
+pub trait IterStatusExt: Iterator + Sized {
+    /// Creates an iterator that yields the original items paired with a
+    /// status, which tells you if the item is the first and/or last one.
+    ///
+    /// The new iterator's item has the type `(Self::Item, Status)`. See
+    /// [`Status`] for detailed information.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use splop::IterStatusExt;
+    ///
+    ///
+    /// let mut s = String::new();
+    /// let names = ["anna", "peter", "bob"];
+    ///
+    /// for (name, status) in names.iter().with_status() {
+    ///     if !status.is_first() {
+    ///         s += ", ";
+    ///     }
+    ///
+    ///     s += name;
+    /// }
+    ///
+    /// assert_eq!(s, "anna, peter, bob");
+    /// ```
+    fn with_status(self) -> WithStatus<Self>;
+}
+
+impl<I: Iterator> IterStatusExt for I {
+    fn with_status(self) -> WithStatus<Self> {
+        WithStatus::new(self)
+    }
+}
+
+/// The status of an item from an iterator (e.g. "is this the first item?").
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub struct Status {
+    first: bool,
+    last: bool,
+}
+
+impl Status {
+    /// Returns `true` if this is the first item of the iterator.
+    ///
+    /// Note that an item might simultaniously be the first and last item (if
+    /// the iterator only contains one item). To check if the item is the first
+    /// and and not the last, use [`Status::is_first_only`].
+    pub fn is_first(&self) -> bool {
+        self.first
+    }
+
+    /// Returns `true` if this is the first item and it's not the only item in
+    /// the iterator.
+    pub fn is_first_only(&self) -> bool {
+        self.first && !self.last
+    }
+
+    /// Returns `true` if this is the last item of the iterator.
+    ///
+    /// Note that an item might simultaniously be the last and first item (if
+    /// the iterator only contains one item). To check if the item is the last
+    /// and and not the first, use [`Status::is_last_only`].
+    pub fn is_last(&self) -> bool {
+        self.last
+    }
+
+    /// Returns `true` if this is the last item and it's not the only item in
+    /// the iterator.
+    pub fn is_last_only(&self) -> bool {
+        self.first && !self.last
+    }
+
+
+    /// Returns `true` if this is neither the first nor the last item.
+    pub fn is_in_between(&self) -> bool {
+        !self.first && !self.last
     }
 }
